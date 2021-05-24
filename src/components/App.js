1e6eb5949a4674e38491ea94ae8b8c2cbae59685
Luis Vinicius <luisvinicius0906@gmail.com>
2021-05-24 20:58:27 -0300

    Update App component that render the Display and the Buttons Panels

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,12 @@
+import React from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 
-export default class App {
-  render() {
-    return (
-      <div>
-        <h1>Hello to the real world</h1>
-        <Display />
-        <ButtonPanel />
-      </div>
-    )
-  }
-}
+const App = () => (
+  <div>
+    <Display />
+    <ButtonPanel />
+  </div>
+);
+
+export default App;
