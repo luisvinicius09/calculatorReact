a7b01228807b9ee4ff755ecdae666d40705115fc
Luis Vinicius <luisvinicius0906@gmail.com>
2021-05-25 20:02:11 -0300

    Create functionality to use percentage

--- src/utils/calculate.js
+++ src/utils/calculate.js
@@ -22,6 +22,42 @@ const calculate = ({ total, next, operation }, button) => {
       }
       return {};
     case '%':
+      if (total && !next) {
+        if (!operation) {
+          return {
+            total: Big(total).div(100).toString(),
+            next,
+            operation
+          };
+        }
+        return {
+          total,
+          next: Big(total).div(100).toString(),
+          operation,
+        }
+      }
+      if (total && next) {
+        if (operation === '+' || operation === '-') {
+          const modifier = Big(next).div(100).times(total).toString();
+          return {
+            total,
+            next: modifier,
+            operation,
+          };
+        } else if (operation === 'x' || operation === '÷') {
+          const modifier = Big(next).div(100).toString();
+          return {
+            total,
+            next: modifier,
+            operation,
+          };
+        };
+      }
+      return {
+        total,
+        next,
+        operation
+      };
     case operation: 
     case '=': 
     case '.':
