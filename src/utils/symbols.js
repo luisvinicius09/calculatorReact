be6d6828c92f0eb086afc1d526f6c1cf64a5a676
Luis Vinicius <luisvinicius0906@gmail.com>
2021-05-30 15:11:16 -0300

    Add symbols file to create individuatily to buttons

---
+++ src/utils/symbols.js
@@ -0,0 +1,80 @@
+const symbols = [
+  {
+    symbol: 'A/C',
+    type: 'operator',
+  },
+  {
+    symbol: '+/-',
+    type: 'operator',
+  },
+  {
+    symbol: '%',
+    type: 'operator',
+  },
+  {
+    symbol: '÷',
+    type: 'operator',
+  },
+  {
+    symbol: '1',
+    type: 'num',
+  },
+  {
+    symbol: '2',
+    type: 'num',
+  },
+  {
+    symbol: '3',
+    type: 'num',
+  },
+  {
+    symbol: 'x',
+    type: 'operator',
+  },
+  {
+    symbol: '4',
+    type: 'num',
+  },
+  {
+    symbol: '5',
+    type: 'num',
+  },
+  {
+    symbol: '6',
+    type: 'num',
+  },
+  {
+    symbol: '+',
+    type: 'operator',
+  },
+  {
+    symbol: '7',
+    type: 'num',
+  },
+  {
+    symbol: '8',
+    type: 'num',
+  },
+  {
+    symbol: '9',
+    type: 'num',
+  },
+  {
+    symbol: '-',
+    type: 'operator',
+  },
+  {
+    symbol: '0',
+    type: 'num',
+  },
+  {
+    symbol: '.',
+    type: 'operator',
+  },
+  {
+    symbol: '=',
+    type: 'operator',
+  },
+];
+
+export default symbols;
