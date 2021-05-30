f2c9ed8b80996f34b5252d4aef83c4ddc8139341
Luis Vinicius <luisvinicius0906@gmail.com>
2021-05-30 15:03:19 -0300

    Update logic to render buttons

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -4,36 +4,18 @@ import Button from './Button';
 const ButtonPanel = ({ clickHandler }) => {
   const handleClick = (buttonName) => clickHandler(buttonName);
 
+  const symbols = [
+    'A/C', '+/-', '%', '÷',
+    '1', '2', '3', 'x',
+    '4', '5', '6', '+',
+    '7', '8', '9', '-',
+    '0', '.', '=',
+  ];
+
   return (
     <>
       <div>
-        <Button value="A/C" clickHandler={handleClick} />
-        <Button value="+/-" clickHandler={handleClick} />
-        <Button value="%" clickHandler={handleClick} />
-        <Button value="÷" clickHandler={handleClick} />
-      </div>
-      <div>
-        <Button value="1" clickHandler={handleClick} />
-        <Button value="2" clickHandler={handleClick} />
-        <Button value="3" clickHandler={handleClick} />
-        <Button value="x" clickHandler={handleClick} />
-      </div>
-      <div>
-        <Button value="4" clickHandler={handleClick} />
-        <Button value="5" clickHandler={handleClick} />
-        <Button value="6" clickHandler={handleClick} />
-        <Button value="+" clickHandler={handleClick} />
-      </div>
-      <div>
-        <Button value="7" clickHandler={handleClick} />
-        <Button value="8" clickHandler={handleClick} />
-        <Button value="9" clickHandler={handleClick} />
-        <Button value="-" clickHandler={handleClick} />
-      </div>
-      <div>
-        <Button value="0" clickHandler={handleClick} />
-        <Button value="." clickHandler={handleClick} />
-        <Button value="=" clickHandler={handleClick} />
+        {symbols.map((s) => <Button value={s} key={s} clickHandler={handleClick} />)}
       </div>
     </>
   );
