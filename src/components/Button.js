479eb8f4e60b960ce80b15f1a6ab6dee17aa7001
Luis Vinicius <luisvinicius0906@gmail.com>
2021-05-30 18:42:54 -0300

    Add style to individual buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,22 @@
 import PropTypes from 'prop-types';
 
-const Button = ({ value, clickHandler }) => {
+const Button = ({
+  value, clickHandler, color, wide,
+}) => {
   const handleClick = () => clickHandler(value);
+
+  const size = wide ? '1 1 50%' : '1 1 25%';
+
+  const styles = {
+    color: 'black',
+    border: '1px solid black',
+    backgroundColor: color,
+    flex: size,
+  };
 
   return (
     <>
-      <button type="button" onClick={handleClick}>{value}</button>
+      <button style={styles} type="button" onClick={handleClick}>{value}</button>
     </>
   );
 };
@@ -15,4 +26,6 @@ export default Button;
 Button.propTypes = {
   value: PropTypes.string.isRequired,
   clickHandler: PropTypes.func.isRequired,
+  color: PropTypes.string.isRequired,
+  wide: PropTypes.bool.isRequired,
 };
