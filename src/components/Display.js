d9a14011b27acfbadeb001d5855717ffe416921f
Luis Vinicius <luisvinicius0906@gmail.com>
2021-05-30 21:18:06 -0300

    Add styling to display

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,10 +1,22 @@
 import PropTypes from 'prop-types';
 
-const Display = ({ result }) => (
-  <div>
-    <h1>{result}</h1>
-  </div>
-);
+const Display = ({ result }) => {
+  const styles = {
+    display: 'flex',
+    height: '100px',
+    backgroundColor: 'grey',
+    color: 'white',
+    padding: '0 10px 0 10px',
+    textAlign: 'right',
+    flexDirection: 'column',
+  };
+
+  return (
+    <div style={styles}>
+      <h1>{result}</h1>
+    </div>
+  );
+};
 
 export default Display;
 
