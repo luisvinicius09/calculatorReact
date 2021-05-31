12bf3eab8adf9abbe31baf1bb503dcc6eeb193e0
Luis Vinicius <luisvinicius0906@gmail.com>
2021-05-31 01:40:02 -0300

    Add navbar component

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import NavBar from './Navbar';
+
+const Home = () => (
+  <>
+    <NavBar />
+    <h1>Hello from the real world 2</h1>
+  </>
+);
+
+export default Home;
---
+++ src/components/Quote.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import NavBar from './Navbar';
+
+const Quote = () => (
+  <>
+    <NavBar />
+    <h1>Hello from the real world 3</h1>
+  </>
+);
+
+export default Quote;
