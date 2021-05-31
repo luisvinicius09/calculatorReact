775102f8bc2396e1cebb7d9654017bcb41d5481e
Luis Vinicius <luisvinicius0906@gmail.com>
2021-05-31 01:49:11 -0300

    Add loren text

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -4,7 +4,16 @@ import NavBar from './Navbar';
 const Quote = () => (
   <>
     <NavBar />
-    <h1>Hello from the real world 3</h1>
+    <div style={{ margin: '50px', textAlign: 'center' }}>
+      <h1>Hello from the real world</h1>
+      <p>
+        What you know you can&#39;t explain, but you feel it.
+        <br />
+        You&#39;ve felt it your entire life, that there&#39;s something wrong with the world.
+        You don&#39;t know what it is, but it&#39;s there,
+        like a splinter in your mind, driving you mad.
+      </p>
+    </div>
   </>
 );
 
